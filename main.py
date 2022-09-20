a3295b1ed63b9e1468a8e7374c098d8a2e1a403b
xuliji <2384465976@qq.com>
2022-09-20 14:28:38 +0800

    update

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ file_list = np.load('picked_uiuc.npy', allow_pickle=True)
 
 # coarse mesh
 for airfoil_file in tqdm(file_list, desc="Get Coarse Mesh:"):
-    get_mesh(airfoil_file, mesh_size=0.01, store_path='./Meshes/fine', optimizer=False)
-    get_config(airfoil_file, store_path='./Meshes/fine', template_path='inv_NACA0012.cfg')
+    get_mesh(airfoil_file, mesh_size=0.01, store_path='./Meshes/coarse', optimizer=False)
+    get_config(airfoil_file, store_path='./Meshes/coarse', template_path='inv_NACA0012.cfg')
