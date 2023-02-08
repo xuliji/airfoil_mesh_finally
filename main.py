2575e22712f65d0e598612323c2d6d4f230b6d7f
xuliji <2384465976@qq.com>
2023-02-08 14:20:34 +0800

    update

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from tqdm import tqdm
 
 # coarse mesh
 for i in tqdm(range(500), desc="Get Coarse Mesh:"):
-    # get_mesh('MMOE_MFM_test_rae{}.dat'.format(str(i)), mesh_size=0.01, store_path='./RAE_Mesh/fine', optimizer=False)
-    # get_mesh('MMOE_MFM_test_rae{}.dat'.format(str(i)), mesh_size=0.1, store_path='./RAE_Mesh/coarse', optimizer=False)
-    # get_config('MMOE_MFM_test_rae{}.dat'.format(str(i)), store_path='./RAE_Mesh/coarse', template_path='inv_NACA0012.cfg')
-    # get_config('MMOE_MFM_test_rae{}.dat'.format(str(i)), store_path='./RAE_Mesh/fine', template_path='inv_NACA0012.cfg')
+    get_mesh('MMOE_MFM_test_rae{}.dat'.format(str(i)), mesh_size=0.01, store_path='./RAE_Mesh/fine', optimizer=False)
+    get_mesh('MMOE_MFM_test_rae{}.dat'.format(str(i)), mesh_size=0.1, store_path='./RAE_Mesh/coarse', optimizer=False)
+    get_config('MMOE_MFM_test_rae{}.dat'.format(str(i)), store_path='./RAE_Mesh/coarse', template_path='inv_NACA0012.cfg')
+    get_config('MMOE_MFM_test_rae{}.dat'.format(str(i)), store_path='./RAE_Mesh/fine', template_path='inv_NACA0012.cfg')
     # Computer('MMOE_MFM_test_rae{}.dat'.format(str(i)), work_path='./RAE_Mesh/coarse')
-    Computer('MMOE_MFM_test_rae{}.dat'.format(str(i)), work_path='./RAE_Mesh/fine')
+    # Computer('MMOE_MFM_test_rae{}.dat'.format(str(i)), work_path='./RAE_Mesh/fine')
     # break
